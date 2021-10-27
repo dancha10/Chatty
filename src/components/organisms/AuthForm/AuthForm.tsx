88dcd9b03c006d05239a654826207624a53cd8cc
Daniil Abramenko <mdan201510@gmail.com>
2021-10-28 01:05:22 +0700

    feat: add organism

--- src/components/organisms/AuthForm/AuthForm.tsx
+++ src/components/organisms/AuthForm/AuthForm.tsx
@@ -1,27 +1,63 @@
 import React from 'react';
-import InputForm from '../../molecules/InputForm/InputForm';
-import Button from '../../atoms/button/button';
+import { useForm, SubmitHandler } from 'react-hook-form';
+import { FormField } from '../../molecules/FormField/FormField';
+import { Button } from '../../atoms/Button/button';
 import './style.scss';
 
-const AuthForm = () => {
+type InputValidatorType = {
+  name: string;
+  pass: string;
+};
+
+export const AuthForm = () => {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<InputValidatorType>();
+  // eslint-disable-next-line no-console
+  const onSubmit: SubmitHandler<InputValidatorType> = data => console.log(data);
+
   return (
-    <form action="#" method="POST" className="AuthForm">
-      <div className="AuthForm__login">
-        <InputForm placeholder="Input user name" forHtml="name" id="name" text="User name" />
+    <form className="auth-form" onSubmit={handleSubmit(onSubmit)}>
+      <div className="auth-form__login">
+        <FormField
+          placeholder="Input user name"
+          forHtml="name"
+          id="name"
+          text="User name"
+          InputValidator={{
+            ...register('name', {
+              required: { value: true, message: 'This is field required' },
+              minLength: { value: 2, message: 'Please enter more than 2 characters' },
+              maxLength: { value: 30, message: 'Max length exceeded' },
+              pattern: { value: /^[A-Za-zA-Яа-яЁё ]+$/i, message: 'The entered data is incorrect' },
+            }),
+          }}
+          isError={errors.name && true}
+        />
+        {/* Только после того как нагородил это, узнал про yup.... */}
+        {errors.name && <span className="auth-form__error">{errors.name.message}</span>}
       </div>
-      <div className="AuthForm__password">
-        <InputForm
+      <div className="auth-form__password">
+        <FormField
           type="password"
           placeholder="Input password"
           forHtml="pass"
           id="pass"
           text="Password"
-          isError
+          InputValidator={{
+            ...register('pass', {
+              required: { value: true, message: 'This is field required' },
+              minLength: { value: 4, message: 'Please enter more than 4 characters' },
+              maxLength: { value: 25, message: 'Max length exceeded' },
+            }),
+          }}
+          isError={errors.pass && true}
         />
+        {errors.pass && <span className="auth-form__error">{errors.pass.message}</span>}
       </div>
       <Button type="submit" disabled={false} onClick={() => {}} text="Log In" />
     </form>
   );
 };
-
-export default AuthForm;
